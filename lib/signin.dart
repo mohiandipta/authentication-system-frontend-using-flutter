6c72b735f94342d6c5b9e517ddab9a44c1a0d032
mohiandipta <mohianmostofa@gmail.com>
2021-07-18 13:03:51 +0600

    built stucture from scratch

---
+++ lib/signin.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Signin extends StatefulWidget {
+  @override
+  _SigninState createState() => _SigninState();
+}
+
+class _SigninState extends State<Signin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text('Signin'),
+        ),
+      ),
+    );
+  }
+}
