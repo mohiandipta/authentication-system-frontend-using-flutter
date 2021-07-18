329c386d445cbb84daf4380b22cc0a9ce1ffafcd
mohiandipta <mohianmostofa@gmail.com>
2021-07-18 19:40:23 +0600

    svg added to the bottom

--- lib/signin.dart
+++ lib/signin.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'styles/my_svg.dart';
 
 class Signin extends StatefulWidget {
   @override
@@ -9,10 +11,25 @@ class _SigninState extends State<Signin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Signin'),
-        ),
+      body: Stack(
+        children: [
+          Positioned(
+            bottom: 0,
+            child: SvgPicture.asset(
+              top,
+              width: 500,
+              height: 150,
+              color: Colors.lightGreen,
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+            ),
+          )
+        ],
       ),
     );
   }
---
+++ lib/styles/my_svg.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const top = 'images/top.svg';
